6b9c63b06ba6d3de42f1ec9df1c2164ca3571a23
Ben <BenKalegin@users.noreply.github.com>
2025-01-12 10:38:30 -0500

    delete current cell by Del or Backspace

--- src/components/Spreadsheet.tsx
+++ src/components/Spreadsheet.tsx
@@ -39,6 +39,16 @@ const Spreadsheet: React.FC = () => {
                 case 'ArrowRight':
                     col = Math.min(col + 1, MAX_COLS - 1);
                     break;
+
+                case 'Delete':
+                case 'Backspace':
+                    setCellData((prev) => {
+                        const newData = { ...prev };
+                        delete newData[`${row},${col}`];
+                        return newData;
+                    });
+                    break;
+
                 case 'Enter':
                 case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n': case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
                 case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G': case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': case 'N': case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z':
